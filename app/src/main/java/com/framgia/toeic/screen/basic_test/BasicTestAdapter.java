00f0b9adf04928d3d883c22d8b2142a8c3fe69a4
thanh <thanh01684265170@gmail.com>
2019-02-12 15:41:14 +0700

    edit raeting

--- app/src/main/java/com/framgia/toeic/screen/basic_test/BasicTestAdapter.java
+++ app/src/main/java/com/framgia/toeic/screen/basic_test/BasicTestAdapter.java
@@ -3,7 +3,6 @@ package com.framgia.toeic.screen.basic_test;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
